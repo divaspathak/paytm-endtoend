b16d7c4aefac1a809333e02dd07892583007f790
Divas Pathak <divaspathak@gmail.com>
2024-01-25 12:38:22 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const { mongoose } = require("mongoose");
+mongoose.connect("mongodb+srv://divaspathak:Divas5563**@cluster0.2n3lmsk.mongodb.net/Paytm?retryWrites=true&w=majority")
+
+const userSchema = new mongoose.Schema({
+    username : String, 
+    password : String,
+    firstname : String, 
+    lastname : String, 
+})
+
+const User = mongoose.model("User", userSchema); 
+
+module.exports = {
+    User, 
+}
